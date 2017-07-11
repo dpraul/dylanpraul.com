4438565a2b8a760575ec4caf8f0a616621db15b2
Dylan Praul <dylan.praul@gmail.com>
2017-07-11 00:02:05 -0400

    MC site

---
+++ mc/config.js
@@ -0,0 +1,52 @@
+var CONFIG = {
+  "maps": {
+    "map_myworld": {
+      "imageFormat": "jpg",
+      "lastRendered": [
+        1499745243,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 10,
+      "name": "My World",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "myworld_isometric_t1",
+      "tileSize": 384,
+      "world": "myworld",
+      "worldName": "myworld",
+      "worldSeaLevel": 64
+    }
+  },
+  "mapsOrder": [
+    "map_myworld"
+  ],
+  "tileSetGroups": {
+    "myworld_isometric_t1": {
+      "maxZoom": 10,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ],
+      "tileWidth": 1
+    }
+  }
+};
